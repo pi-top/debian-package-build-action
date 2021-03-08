1e6c0fba482890077891f2e7e5da6039c678ad60
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-03-08 19:54:45 +0000

    Work out of temporary directory

--- build-deb.sh
+++ build-deb.sh
@@ -7,10 +7,15 @@ set -euo pipefail
 IFS=$'\n\t'
 ###############################################################
 
-echo "[build-deb] Changing working directory to source... (/src)"
-cd /src
+echo "[build-deb] Copying source files to temporary directory (${tmp_dir_root})..."
+tmp_dir_root=$(mktemp -d)
+tmp_dir=$(mkdir ${tmp_dir_root}/src)
+cp -r /src/* "${tmp_dir}/"
 
-echo "[build-deb] DEBUG: Listing directory contents..."
+echo "[build-deb] Changing working directory to temporary directory..."
+cd "${tmp_dir}"
+
+echo "[build-deb] DEBUG: Listing temporary directory contents..."
 ls -l
 
 echo "[build-deb] Updating package list..."
@@ -19,22 +24,19 @@ apt-get update
 echo "[build-deb] Installing build dependencies..."
 apt-get build-dep -y .
 
-echo "[build-deb] Moving source files to temporary directory..."
-tmp_dir_root=$(mktemp -d)
-tmp_dir=$(mkdir ${tmp_dir_root}/src)
-cp -r /src/* "${tmp_dir}/"
-
-cd "${tmp_dir}"
 echo "[build-deb] Building package..."
 # No GPG signing
 # Skip checking build dependencies (can fail erroneously)
 dpkg-buildpackage --no-sign --no-check-builddeps --post-clean
 
-echo "[build-deb] Moving build files..."
+echo "[build-deb] Moving build files to /build..."
 for x in "${tmp_dir_root}/"*; do
    if ! [ -d "$x" ]; then
      mv -- "$x" /build
    fi
 done
+
+echo "[build-deb] DEBUG: Listing /build directory contents..."
+ls -l /build
 
 echo "[build-deb] DONE!"
