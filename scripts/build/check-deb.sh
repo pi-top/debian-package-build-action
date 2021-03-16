181d3097790b385297a0cd2b7f7ba94097a67f47
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-03-16 23:23:57 +0000

    Find .changes file in build directory

--- scripts/build/check-deb.sh
+++ scripts/build/check-deb.sh
@@ -18,9 +18,18 @@ cd /build
 debug_echo "[check-deb] DEBUG: print LINTIAN_OPTS..."
 debug_echo "${LINTIAN_OPTS}"
 
-debug_echo "[check-deb] Running Lintian..."
+debug_echo "[check-deb] Parsing Lintian arguments..."
 IFS=' ' read -ra LINTIAN_OPTS_ARR <<< "$LINTIAN_OPTS"
-lintian "${LINTIAN_OPTS_ARR[@]}" | tee "${LINTIAN_LOG_FILE}"
+
+debug_echo "[check-deb] Checking for .changes file..."
+changes_file="$(find . -name "*.changes" | head -n1)"
+if [[ -f "${changes_file}" ]]; then
+ echo "[check-deb] ERROR: No .changes file found."
+ exit 1
+fi
+
+debug_echo "[check-deb] Running Lintian..."
+lintian "${LINTIAN_OPTS_ARR[@]}" "${changes_file}" | tee "${LINTIAN_LOG_FILE}"
 
 if [[ "${DEBUG}" -eq 1 ]]; then
   lintian-info "${LINTIAN_LOG_FILE}"
