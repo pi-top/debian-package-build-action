5b5aa9ef2102fea076959acc0a62ff60b125838f
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-03-17 14:49:49 +0000

    Insecure curl of pi-top key

--- scripts/setup/install-dev-packages.sh
+++ scripts/setup/install-dev-packages.sh
@@ -15,30 +15,29 @@ dev_packages=("sudo" "dpkg-dev" "debhelper" "lintian" "fakeroot")
 # Tell apt-get we're never going to be able to give manual feedback
 export DEBIAN_FRONTEND=noninteractive
 
-# Update the package listing, so we know what packages exist
+echo "Updating package list..."
 apt-get update
 
-# Install wget and gnupg for getting/installing APT key
+echo "Installing tools to get/install APT key..."
 apt-get -y -t buster-backports install --no-install-recommends "${support_packages[@]}"
 
-# Add Raspberry Pi's repo
+echo "Adding Raspberry Pi's repo..."
 curl -fsSL http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
 echo "deb http://archive.raspberrypi.org/debian/ buster main" > /etc/apt/sources.list.d/raspi.list
 
-# Add pi-top's repo
-curl https://apt.pi-top.com/pt-apt.asc | apt-key add
+echo "Adding pi-top's repo..."
+curl --insecure http://apt.pi-top.com/pt-apt.asc | apt-key add
 echo "deb http://apt.pi-top.com/pi-top-os sirius main contrib non-free" > /etc/apt/sources.list.d/pi-top.list
 
-# Update the package listing, so we know what package exist
+echo "Updating package list..."
 apt-get update
 
-# Install security updates
+echo "Installing security updates..."
 apt-get -y upgrade
 
-# Install new packages, without unnecessary recommended packages
+echo "Installing new packages, without unnecessary recommended packages..."
 apt-get -y -t buster-backports install --no-install-recommends "${dev_packages[@]}"
 
-
-# Delete cached files we don't need anymore
+echo "Deleting cached files we don't need anymore..."
 apt-get clean
 rm -rf /var/lib/apt/lists/*
