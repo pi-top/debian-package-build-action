2c511e9eef1151181c8a3208451d3a2072b8f222
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-03-17 14:12:22 +0000

    Add pi-top repo to sources

--- scripts/setup/install-dev-packages.sh
+++ scripts/setup/install-dev-packages.sh
@@ -21,10 +21,13 @@ apt-get update
 # Install wget and gnupg for getting/installing APT key
 apt-get -y -t buster-backports install --no-install-recommends "${support_packages[@]}"
 
-curl -fsSL http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
-
 # Add Raspberry Pi's repo
+curl -fsSL http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
 echo "deb http://archive.raspberrypi.org/debian/ buster main" > /etc/apt/sources.list.d/raspi.list
+
+# Add pi-top's repo
+curl -fsSL https://apt.pi-top.com/pt-apt.asc | sudo apt-key add
+echo "deb http://apt.pi-top.com/pi-top-os sirius main contrib non-free" > /etc/apt/sources.list.d/pi-top.list
 
 # Update the package listing, so we know what package exist
 apt-get update
