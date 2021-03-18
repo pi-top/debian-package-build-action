b6b2a3eb284685f8c0a765ff4e2d2490b8f9751d
Jorge Capona <jcapona@gmail.com>
2021-03-18 18:26:04 -0300

    Fix: specify architecture of pi-top apt repo

--- scripts/setup/install-dev-packages.sh
+++ scripts/setup/install-dev-packages.sh
@@ -23,11 +23,11 @@ apt-get -y -t buster-backports install --no-install-recommends "${support_packag
 
 echo "Adding Raspberry Pi's repo..."
 curl -fsSL http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
-echo "deb [arch=armhf,amd64,arm64] http://archive.raspberrypi.org/debian/ buster main" > /etc/apt/sources.list.d/raspi.list
+echo "deb http://archive.raspberrypi.org/debian/ buster main" > /etc/apt/sources.list.d/raspi.list
 
 echo "Adding pi-top's repo..."
 curl --insecure https://apt.pi-top.com/pt-apt.asc | apt-key add
-echo "deb http://apt.pi-top.com/pi-top-os sirius main contrib non-free" > /etc/apt/sources.list.d/pi-top.list
+echo "deb [arch=armhf,amd64,arm64] http://apt.pi-top.com/pi-top-os sirius main contrib non-free" > /etc/apt/sources.list.d/pi-top.list
 
 echo "Updating package list..."
 apt-get update
