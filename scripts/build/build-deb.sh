78861a6101de35d67940325f4f2fc0af897770af
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-05-03 13:52:24 +0100

    Attempt to fix tarball package

--- scripts/build/build-deb.sh
+++ scripts/build/build-deb.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 IFS=$'\n\t'
 ###############################################################
 
-
 debug_echo() {
   if [[ "${DEBUG}" -eq 1 ]]; then
     echo "[build-deb] $1"
@@ -45,14 +44,13 @@ debug_echo "DEBUG: print DPKG_BUILDPACKAGE_OPTS..."
 debug_echo "${DPKG_BUILDPACKAGE_OPTS}"
 
 debug_echo "Parsing dpkg-buildpackage arguments..."
-IFS=' ' read -ra DPKG_BUILDPACKAGE_OPTS_ARR <<< "$DPKG_BUILDPACKAGE_OPTS"
+IFS=' ' read -ra DPKG_BUILDPACKAGE_OPTS_ARR <<<"$DPKG_BUILDPACKAGE_OPTS"
 
-version="$(head -n1 debian/changelog | awk '{print $2}' | cut -d'-' -f1 | sed "s/(//g" | sed "s/)//g")"
-if [[ "${version}" == *"-"* ]]; then
-  debug_echo "Package is Debian revision of upstream - creating tarball of source..."
+if ! grep -q "3.0 (native)" ./debian/source/format; then
+  debug_echo "Package is not native Debian package - creating tarball of source..."
 
   source_package="$(head -n1 debian/changelog | awk '{print $1}')"
-  upstream_version="$(echo "${version}" | cut -d'-' -f1)"
+  upstream_version="$(dpkg-parsechangelog -Sversion | cut -d'-' -f1)"
   tar \
     --exclude-vcs \
     --exclude ./debian \
@@ -60,7 +58,7 @@ if [[ "${version}" == *"-"* ]]; then
     -C /src \
     ./
 else
-  debug_echo "Package is not Debian revision of upstream - skipping tarball..."
+  debug_echo "Package is native Debian package - skipping tarball..."
 fi
 
 debug_echo "Building package..."
@@ -73,9 +71,9 @@ fi
 
 debug_echo "Moving build files to /build..."
 for x in "${tmp_dir_root}/"*; do
-   if ! [ -d "$x" ]; then
-     sudo mv -- "$x" /build
-   fi
+  if ! [ -d "$x" ]; then
+    sudo mv -- "$x" /build
+  fi
 done
 
 debug_echo "DEBUG: Listing /build directory contents..."
